c3d174b16f022d8ec44ea72c2a976566a30d0598
basma <basmasabry33333@gmail.com>
2025-05-26 17:00:39 +0300

    fix: Update author name in __init__.py

--- ms-loader/app/__init__.py
+++ ms-loader/app/__init__.py
@@ -4,7 +4,7 @@ A microservice for loading and processing Call Detail Records (CDR)
 """
 
 __version__ = "1.0.0"
-__author__ = "Your Name"
+__author__ = "Basma Sabry"
 
 from app.scheduling.scheduler import run, job
 from app.parsers import parse_all_files
