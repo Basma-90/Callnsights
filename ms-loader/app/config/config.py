b8a3112c0e88e7c389b101ee764cf1918ee494c4
basma <basmasabry33333@gmail.com>
2025-06-04 20:54:27 +0300

    feat: Add Docker and Kubernetes configurations for ms-loader service, including PostgreSQL and Kafka setup

--- ms-loader/app/config/config.py
+++ ms-loader/app/config/config.py
@@ -4,15 +4,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 POSTGRES = {
-    "host": os.getenv("POSTGRES_HOST", "localhost"),
-    "port": os.getenv("POSTGRES_PORT", "5432"),
+    "host": os.getenv("POSTGRES_HOST", "postgres.kafka.svc.cluster.local"),
+    "port": int(os.getenv("POSTGRES_PORT", "5432")),
     "user": os.getenv("POSTGRES_USER", "postgres"),
     "password": os.getenv("POSTGRES_PASSWORD", "password"),
     "db": os.getenv("POSTGRES_DB", "postgres"),
 }
 
 KAFKA = {
-    "bootstrap_servers": os.getenv("KAFKA_SERVERS", "localhost:29092"),
+    "bootstrap_servers": os.getenv("KAFKA_SERVERS", "ms-kafka-simple-kafka-bootstrap.kafka.svc.cluster.local:9092"),
     "topic": os.getenv("KAFKA_TOPIC", "cdr-records"),
     "group_id": os.getenv("KAFKA_GROUP_ID", "cdr-consumer-group"),
     "ssl": os.getenv("KAFKA_SSL", "false").lower() == "true",
